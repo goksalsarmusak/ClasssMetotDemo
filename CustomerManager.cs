686ce206313e5778cad95ad32dc153b505fc2f01
goksalsarmusak <77418553+goksalsarmusak@users.noreply.github.com>
2021-01-20 20:55:12 +0300

    ClassMetotDemo

---
+++ CustomerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Listele(Customer[] customer)
+        {
+            Console.WriteLine("-----Sistemde Kayıtlı Müşteriler-----");
+            foreach (var listCustomer in customer)
+            {
+                Console.WriteLine("{0}-) {1} {2} {3}",listCustomer.Id,listCustomer.Ad,listCustomer.Soyad,listCustomer.HesapNo);
+            }
+        }
+        public void Add(Customer customer)
+        {
+            
+            Console.WriteLine("{0}-) {1} {2} isimli {3} hesap numaralı müşteri başarıyla eklenmiştir."
+                , customer.Id, customer.Ad, customer.Soyad, customer.HesapNo);
+        }
+        public void Delete(Customer customer)
+        {
+            
+            Console.WriteLine("{0} numaralı müşteri başarıyla silinmiştir.", customer.Id);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer()
+            {
+                Id=1,
+                Ad = "Melih",
+                Soyad = "Karakaş",
+                HesapNo =61
+            };
+
+            Customer customer2 = new Customer
+            {
+                Id = 2,
+                Ad="Melike",
+                Soyad="Yıldız",
+                HesapNo=512
+            };
+
+            Customer customer3 = new Customer()
+            {
+                Id=3,
+                Ad="Hamza",
+                Soyad="Şimşek",
+                HesapNo=345
+            };
+
+
+            Customer[] c = new Customer[] {customer1,customer2,customer3 };
+
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Listele(c);
+
+            Console.WriteLine("************************");
+
+            Console.WriteLine("-----Müşteri Ekleme İşlemi-----");
+
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Add(customer3);
+
+            Console.WriteLine("************************");
+
+            Console.WriteLine("-----Müsteri Silme İşlemi");
+            customerManager.Delete(customer1);
+            customerManager.Delete(customer2);
+            customerManager.Delete(customer3);
+        }
+    }
+}
